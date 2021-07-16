5cf7161cdfc7266baf386e45ab159d82222bd149
Matt Novenstern <fisxoj@gmail.com>
2021-07-16 15:10:40 -0400

    Use new function name

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -8,7 +8,7 @@ function fish_right_prompt
 
     if test "$CMD_DURATION" -gt 100
         set -l duration_copy $CMD_DURATION
-        set -l duration (echo $CMD_DURATION | humanize_duration)
+        set -l duration (echo $CMD_DURATION | humantime)
 
          echo -sn (set_color $status_color) "$duration" (set_color normal)
 
